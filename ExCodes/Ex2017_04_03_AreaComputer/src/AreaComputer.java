732eb40054e2e0660e4e6ae109e12fc7ade13f7d
Dknife <young.min.kang@gmail.com>
2017-03-16 15:36:32 +0900

    Area computer

---
+++ ExCodes/Ex2017_04_03_AreaComputer/src/AreaComputer.java
@@ -0,0 +1,13 @@
+
+public class AreaComputer {
+
+	public float areaRect(float w, float h) {
+		return w*h;
+	}
+	public float areaTri(float w, float h) {
+		return 0.5f*w*h;
+	};
+	public float areaCircle(float r) {
+		return 3.14f*r*r;
+	};
+}
