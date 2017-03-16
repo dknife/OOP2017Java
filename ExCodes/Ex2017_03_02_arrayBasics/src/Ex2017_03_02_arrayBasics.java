44d1a3d4f5063a49ec8b729bec4fb4806eeb7f6a
Dknife <young.min.kang@gmail.com>
2017-03-16 10:50:07 +0900

    for and array

---
+++ ExCodes/Ex2017_03_02_arrayBasics/src/Ex2017_03_02_arrayBasics.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex2017_03_02_arrayBasics {
+
+	public static void main(String[] s) {
+		
+		Scanner in = new Scanner(System.in);
+		float numbers[];
+		
+		System.out.print("How many numbers?");
+		int nNumbers = in.nextInt();
+		numbers = new float[nNumbers];
+		for(int i=0;i<nNumbers;i++) {
+			numbers[i] = in.nextFloat();
+		}
+		float sum = 0.0f; float max = numbers[0];
+		for(int i=0;i<nNumbers;i++) {
+			System.out.print(numbers[i]+" ");
+			if(numbers[i]>max) max=numbers[i];
+			sum += numbers[i];
+		}
+		System.out.println(" : average = " + sum/((float)nNumbers));
+		System.out.println(" : max = " + max);
+		
+		
+	}
+}
