edd5de2d57706205b8a0ccd16dd0809710c0c0bd
Dknife <young.min.kang@gmail.com>
2017-03-23 14:49:37 +0900

    inheritance

---
+++ ExCodes/Ex2017_05_03_Inheritance/src/Ex2017_05_03_Inheritance.java
@@ -0,0 +1,10 @@
+
+public class Ex2017_05_03_Inheritance {
+	public static void main(String arg[]) {
+		Shape a = new Shape();
+		System.out.println(a);
+		
+		Triangle b = new Triangle(1.5, 2.0);
+		System.out.println(b);
+	}
+}
