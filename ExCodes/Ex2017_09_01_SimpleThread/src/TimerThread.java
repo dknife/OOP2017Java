dc9daf85b6a075652544072b0bc7c9fc3018b1ae
Dknife <young.min.kang@gmail.com>
2017-04-20 09:47:59 +0900

    thread

---
+++ ExCodes/Ex2017_09_01_SimpleThread/src/TimerThread.java
@@ -0,0 +1,19 @@
+
+public class TimerThread extends Thread {
+	
+	int n = 0;
+	
+	public void run() {
+		while(true) {
+			System.out.println(n);
+			n++;
+			try {
+				sleep(1000);
+			}
+			catch(InterruptedException e) {
+				
+			}
+		}
+	}
+
+}
